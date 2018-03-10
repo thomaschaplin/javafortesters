5fea330df5b9baf92ab7edfc3759f38a1b231380
Thomas Chaplin <35810778+thomaschaplin@users.noreply.github.com>
2018-03-10 20:09:24 +0000

    Initial commit

---
+++ src/test/java/ChapterOne/AClassWithAMethod.java
@@ -0,0 +1,8 @@
+package ChapterOne;
+
+public class AClassWithAMethod {
+
+    public void aMethodOnAClass(){
+        System.out.println("Hello World");
+    }
+}
---
+++ src/test/java/ChapterOne/AnEmptyClass.java
@@ -0,0 +1,4 @@
+package ChapterOne;
+
+public class AnEmptyClass {
+}
